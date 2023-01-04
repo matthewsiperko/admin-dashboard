ad624f5a3f596ebfc14085fa8eb758600c2428c5
unknown <matthewsiperko@gmail.com>
2023-01-04 15:50:34 -0500

    finished server setup and connected to database via mongoose

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
 import helmet from 'helmet'
+import clientRoutes from './routes/client.js'
+import generalRoutes from './routes/general.js'
+import managementRoutes from './routes/management.js'
+import salesRoutes from './routes/sales.js'
 
 // CONFIGURATION //
 dotenv.config()
@@ -16,3 +20,23 @@ app.use(morgan("common"))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
+
+// ROUTES //
+app.use("/client", clientRoutes)
+app.use("/general", generalRoutes)
+app.use("/management", managementRoutes)
+app.use("sales", salesRoutes)
+
+// MONGOOSE SETUP //
+const port = process.env.PORT || 9000
+mongoose.set("strictQuery", false)
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(port, () => {
+        console.log(`Server Port ${port}`)
+    })
+}).catch((error) => {
+    console.log(`${error} could not connect`)
+})
